a2e93ed6f8c59ce9ccb08c91b618c754a42da596
Martin Harrigan <martinharrigan@gmail.com>
2012-11-04 12:31:00 +0000

    Add custom classes for notice, error and alert

--- app/helpers/humane_flash_messages_helper.rb
+++ app/helpers/humane_flash_messages_helper.rb
@@ -2,13 +2,13 @@ module HumaneFlashMessagesHelper
   def humane_flash_messages
     content = ""
     unless flash[:notice].blank?
-      content << javascript_tag("humane.info(\"#{flash[:notice]}\")")
+      content << javascript_tag("humane.notice(\"#{flash[:notice]}\")")
     end
     unless flash[:error].blank?
       content << javascript_tag("humane.error(\"#{flash[:error]}\")")
     end
     unless flash[:alert].blank?
-      content << javascript_tag("humane.error(\"#{flash[:alert]}\")")
+      content << javascript_tag("humane.alert(\"#{flash[:alert]}\")")
     end
     content
   end
